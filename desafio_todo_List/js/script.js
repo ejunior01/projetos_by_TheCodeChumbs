c794a33b8cf7384248ff978cf1b4085938b6c6c7
Edvan Ribeiro <jk.jr47@gmail.com>
2020-11-01 19:06:45 -0300

    Desafio Todo

--- desafio_todo_List/js/script.js
+++ desafio_todo_List/js/script.js
@@ -1,31 +1,38 @@
-// Função utilizada para contar a quantidade de Caracteres, e alterar a cor confore uma quantidade.
-var x = document.getElementById("input-text").value
+const listToDo = document.querySelector(".listToDo");
+const createToDo = document.querySelector(".createToDo label");
+const valueToDo = document.querySelector(".createToDo input");
 
-function letter_length() {
-    var x = document.getElementById("input-text").value.trim()
-
-    document.getElementById("p-input").innerHTML = x.length
-
-    if (x.length < 15) {
-        document.getElementById("header-color").style.backgroundColor = "#19A6FF"
-    }
-    else if (x.length < 25) {
-        document.getElementById("header-color").style.backgroundColor = "#FFFC19"
-    }
-    else if (x.length < 35) {
-        document.getElementById("header-color").style.backgroundColor = "#0971B3"
-    }
-    else if (x.length < 45) {
-        document.getElementById("header-color").style.backgroundColor = "#FFFC19"
-    }
-    else if (x.length > 55) {
-        document.getElementById("header-color").style.backgroundColor = "#FF0201"
-   }
+function createElement() {
+  let containerToDo = document.createElement("div");
+  containerToDo.classList.add("todo");
+  containerToDo.innerHTML = `<p class="style-todo"></p><span class="button remove" onClick='removeTodo(event)'>REMOVE</span>`;
+  return containerToDo;
 }
 
-function limpar() {
-    document.getElementById("input-text").value = ''
-    document.getElementById("p-input").innerHTML = ''
-    document.getElementById("header-color").style.backgroundColor = "#FF0201";
+function addElement(todo) {
+  todo.querySelector("p").innerText = valueToDo.value;
+  listToDo.appendChild(todo);
 }
 
+function removeTodo(event) {
+  const todo = event.currentTarget;
+  todo.parentElement.remove();
+}
+
+createToDo.addEventListener("click", () => {
+  if (!valueToDo.value == "") {
+    const newToDo = createElement();
+    addElement(newToDo);
+    valueToDo.value = "";
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (!valueToDo.value == "") {
+    if (e.code == "Enter") {
+      const newToDo = createElement();
+      addElement(newToDo);
+      valueToDo.value = "";
+    }
+  }
+});
